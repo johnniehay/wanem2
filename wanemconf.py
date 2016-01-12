641725c0ccaee6e2ca2b9df9b181ab9f5f199b5d
johnniehay <you@example.com>
2016-01-12 15:03:53 +0200

    Initial commit
    First commit

---
+++ wanemconf.py
@@ -0,0 +1,91 @@
+
+#	^(\d*)\s*(\w*[ ])?(\d*[ ])?(?:(\w*)=(\w*);?)+\s*[/]*.*$
+#	^(\d*)\s*(\w*[ ])?(\d*[ ])?([^ ]*)\s*[/]*.*$
+#	^(\d*)\s*(\w*)[ ]?(\d*)\s*([^ ]*).*$
+
+import re
+import csv
+import sys
+
+dkey = {	"delay" 	:	"txtDelay",
+	 	"loss"		:	"txtLoss",
+	 	"jitter"	:	"txtDelayJitter",
+		"bandwidth"	:	"txtBandwidth"
+	}
+if len(sys.argv) != 3:
+	print "Usage: wanemconf inputconf outputcsv"
+	
+with open(sys.argv[1],'rb') as inf:
+	conf = inf.readlines()
+	print "Input confugration:"
+	c = []
+	for line in conf:
+		print "\t",line,
+		m = re.match(r"^([\d.]*)\s*(\w*)[ ]?(\d*)\s*(\S*).*$", line)
+		cl = list(m.groups())
+		if cl[1] == "command":
+			#print "command:", line
+			line = line.rstrip("\n")
+			cl[2] = line[line.find(" command ")+len(" command "):]
+			if cl[2].startswith('"') and cl[2].endswith('"'):
+   				cl[2] = cl[2][1:-1]
+				#print cl[2]
+			cl[3] = [ ]
+		
+		if cl[3]:
+			cl[3] = cl[3].split(';')
+			# Split and validate the line 
+			for part in range(len(cl[3])):
+				cl[3][part] = cl[3][part].split("=")
+			#print cl
+		c.append(cl)
+	c.sort(key=lambda x:float(x[0]))
+	#print c
+	
+headlist = ["time","interface","subconfig"]
+for l in range(len(c)):
+	for p in range(len(c[l][3])):				# Substitute keywords for html form control names using d
+		if c[l][3][p][0] in dkey:
+			c[l][3][p][0] = dkey[c[l][3][p][0]]
+			if c[l][3][p][0] not in headlist:
+				headlist.append(c[l][3][p][0])
+		else:
+			if c[l][3]:
+				print "Parameter %s not found" % repr(c[l][3][p][0])
+#		print c[l][3][p][0]
+#print c
+
+#print headlist
+parmlist = []
+
+
+for l in c:										#Build csv dictionary
+	if l[3]:
+		d = dict(l[3])
+	else:
+		d = dict()
+	d["time"] = l[0]
+	d["interface"] = l[1]
+	d["subconfig"] = l[2]
+	if not l[2]:
+		d["subconfig"] = "1"
+	parmlist.append(d)
+	#print d
+	#for p in headlist:
+	#	if next((x[1] for x in l[3] if p in x[0]), None):
+	#		print "Got:", p , "in", l
+	
+#print parmlist;
+	
+with open(sys.argv[2],"wb") as outf:			# Write to csv file
+    writer = csv.DictWriter(outf, fieldnames=headlist)
+    writer.writeheader()
+    for di in parmlist:
+        writer.writerow(di)
+		
+print "Output configuration:"
+with open(sys.argv[2],"rb") as outf:			# Read out csv file
+	outc = outf.readlines()
+	for l in outc:
+		print "\t",l,
+print "Written to", sys.argv[2]
---
+++ wanemrunfast.py
@@ -0,0 +1,104 @@
+import os
+import re
+import csv
+import sys
+import time
+import mechanize
+starttime = time.time()
+early = 0.2
+if len(sys.argv) != 3:
+	print "Usage: wanemrun serverhostname|serverip inputcsv"
+
+ifile  = open(sys.argv[2], "rb")
+reader = csv.reader(ifile)
+interface = ""
+br = None
+rownum = 0
+for row in reader:
+	# Save header row.
+	if rownum == 0:
+		header = row
+	else:
+		shcommand = ""
+		colnum = 0
+		for col in row:
+			if colnum == 0:
+				nexttime = float(col)
+				print "nexttime: ", nexttime, starttime+nexttime
+			elif colnum == 1:
+				if col == "command":
+					shcommand = col
+					print "c:" , col
+					colnum += 1
+					continue
+				elif interface != col:
+					if br:
+						br.close()
+					br = mechanize.Browser()
+					br.open("http://"+sys.argv[1]+"/WANem/start_advance.php")
+					br.select_form(nr=0)
+					interface = col
+					br["selInt"] = [col]
+					br.submit()
+				br.select_form(nr=0)
+				print "interface: ", interface
+			elif colnum == 2:
+				if shcommand:
+					print "command:", col
+					shcommand = col
+					break
+				subconf = col
+				if subconf > 1 and (header[3] + subconf) not in br.form.__str__():
+					print "Adding new subconf"
+					br.submit("btnAdd")
+					br.select_form(nr=0)															
+				print "subconf: ", subconf
+			else:
+				if col:
+					if isinstance(br[header[colnum] + subconf], basestring) :
+						br[header[colnum] + subconf] = col
+					else:
+						br[header[colnum] + subconf] = [col]
+					print "br[" + header[colnum] + subconf + "] =" + col 
+			colnum += 1	
+		
+		if starttime+nexttime-early > time.time():
+			print time.time()
+			time.sleep(starttime+nexttime-early-time.time())
+			print time.time()
+
+		
+		
+		#br["txtDelay1"] = "101"
+
+
+		#br.form.find_control("btnApply").readonly = False
+
+		#br.form.find_control("btnAdd").disabled = True
+		#br.form.find_control("btnReset").disabled = True
+		#br.form.find_control("btnRefresh").disabled = True
+		#br.form.find_control("btnStopNetem").disabled = True
+
+		#print br.form
+		import sys, logging
+
+		#logger = logging.getLogger("mechanize")
+		#logger.addHandler(logging.StreamHandler(sys.stdout))
+		#logger.setLevel(logging.DEBUG)
+
+
+		#br.set_debug_http(True)
+		#br.set_debug_responses(True)
+		#br.set_debug_redirects(True)
+		#for control in br.form.controls:
+			#print control.value
+		print time.time()
+		if shcommand:
+			print "Shell running", shcommand
+			os.system(shcommand)
+		else:
+			br.submit("btnApply")
+			print "Submitting:" , time.time(), time.time()-starttime-nexttime
+		#br.close()
+	rownum += 1
+		
